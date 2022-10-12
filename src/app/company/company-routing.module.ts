22afef2554b6d6765067187274ae84d5f87b34e5
Almira Shaikh <almira.shaikh@1Rintern.local>
2022-10-12 13:34:13 +0530

    correction done

--- src/app/company/company-routing.module.ts
+++ src/app/company/company-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompanyFormComponent } from './company-form/company-form.component';
 import { CompanyComponent } from './company.component';
 
 const routes: Routes = [
-  { 
-    path: '', 
-    component: CompanyComponent 
+  {
+    path: '',
+    component: CompanyComponent,
+    children:[
+      {
+        path: 'add',
+        component: CompanyFormComponent
+      }
+    ]
   }
 ];
 
@@ -14,4 +21,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class CompanyRoutingModule { }
- 
